a4a43e37bacdc0513954ae20171086712f5bb6ad
Joshua Oltokun <olatokunjoshua@gmail.com>
2024-03-19 13:21:58 +0100

    Updated authentication middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import User from "../models/User";
+import User from '../models/User';
 import jwt, { JwtPayload } from "jsonwebtoken";
 import dotenv from 'dotenv';
 dotenv.config();
